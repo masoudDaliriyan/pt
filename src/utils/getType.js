54be360694180eca124801b4be9f83c16679ce55
Francisco Hodge <>
2021-03-21 12:18:37 +0000

    Build update

--- src/utils/getType.js
+++ src/utils/getType.js
@@ -1,3 +1,6 @@
-export function getType(value){
-  return ({}).toString.call(value).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+export function getType(value) {
+  return {}.toString
+    .call(value)
+    .match(/\s([a-zA-Z]+)/)[1]
+    .toLowerCase();
 }
